5c81df1b5446677922cc18d830cc515d1658dd1b
Jaaziel Levi <jaaziel@Jaaziels-MacBook-Air.local>
2025-11-06 22:03:46 -0300

    Criação de páginas

--- site-de-livros/src/App.vue
+++ site-de-livros/src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <a class="navbar-brand" href="/">Bibliocat</a>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/cadastroLivro">Cadastro Novo Livros</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/cadastroUsuario">Cadastro Novo Usuário</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/login">Login</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+
+<script setup>
+// Nenhuma lógica extra necessária aqui para o router funcionar
+</script>
+
+<style>
+/* Se quiser um estilo global, pode colocar aqui */
+</style>
